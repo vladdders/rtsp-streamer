2edbaebb798ccdb39e70ce01b7912ad2f347ebce
vladdders <vladsb94@example.com>
2021-03-04 12:07:33 +0200

    support multiple videos

--- feed.py
+++ feed.py
@@ -6,21 +6,27 @@ from gi.repository import Gst, GstRtspServer, GLib
 
 
 PATH_TO_VIDEO = ""
+PATH_TO_VIDEO2 = ""
+PATH_TO_VIDEO3 = ""
 
+
+PATHS = [PATH_TO_VIDEO, PATH_TO_VIDEO2, PATH_TO_VIDEO3]
 
 loop = GLib.MainLoop()
 Gst.init(None)
 
 
 class TestRtspMediaFactory(GstRtspServer.RTSPMediaFactory):
-    def __init__(self):
+    def __init__(self, path_to_video):
         super(TestRtspMediaFactory, self).__init__()
+        self.path_to_video = path_to_video
 
     def do_create_element(self, url):
 
         #set mp4 file path to filesrc's location property
-        src_demux = f"filesrc location={PATH_TO_VIDEO} ! qtdemux name=demux"
-        # h264_transcode = "demux.video_0"
+        src_demux = f"filesrc location={self.path_to_video} ! qtdemux name=demux"
+
+        h264_transcode = "demux.video_0"
         # uncomment following line if video transcoding is necessary
         h264_transcode = "demux.video_0 ! decodebin ! queue ! x264enc"
         pipeline = "{0} {1} ! queue ! rtph264pay name=pay0 config-interval=1 pt=96".format(src_demux, h264_transcode)
@@ -32,10 +38,14 @@ class GstreamerRtspServer():
     def __init__(self):
 
         self.rtspServer = GstRtspServer.RTSPServer()
-        factory = TestRtspMediaFactory()
-        factory.set_shared(True)
-        mountPoints = self.rtspServer.get_mount_points()
-        mountPoints.add_factory("/stream1", factory)
+        
+        factories = [TestRtspMediaFactory(pth) for pth in PATHS]
+
+        for i, factory in enumerate(factories):
+            factory.set_shared(True)
+            mountPoints = self.rtspServer.get_mount_points()
+            mountPoints.add_factory(f"/stream{i}", factory)
+
         self.rtspServer.attach(None)
 
 
